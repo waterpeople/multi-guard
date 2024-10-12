04f41c08064f164b7b67a1678933437f804a9ee0
waterpeople <79010241+waterpeople@users.noreply.github.com>
2024-10-12 10:32:35 +0800

    Add files via upload

---
+++ finetune-multiguard.sh
@@ -0,0 +1,36 @@
+deepspeed LLaVA/llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed  /root/Desktop/LLaVA/scripts/zero2.json \
+    --model_name_or_path /root/Desktop/models/llava-v1.5-7b-finally \
+    --version v1 \
+    --data_path /root/Desktop/LLaVA/playground/data/train_all.json \
+    --image_folder /root/Desktop/mydatasets/mydataset/train/all \
+    --vision_tower /root/Desktop/models/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter /root/Desktop/models/llava-v1.5-7b/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir /root/Desktop/models/llava-v1.5-7b-lora \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 2 \
+    --lazy_preprocess True \
+    --report_to wandb
---
+++ merge-model.sh
@@ -0,0 +1,3 @@
+python /root/Desktop/LLaVA/scripts/merge_lora_weights.py --model-path "/root/Desktop/models/llava-v1.5-7b-lora" \
+       --model-base "/root/Desktop/models/llava-v1.5-7b-finally" \
+       --save-model-path "/root/Desktop/models/llava-v1.5-7b-text-image"
